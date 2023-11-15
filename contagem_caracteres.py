dc95102ae0477115c0eaaeb20ae154770d642e87
Ganziel <100714081+Ganziel@users.noreply.github.com>
2023-11-15 14:04:46 -0300

    Add files via upload

---
+++ contagem_caracteres.py
@@ -0,0 +1,9 @@
+texto = input('Escreva um pequeno texto: ')
+
+lista_texto = [texto]
+
+contagem = len(lista_texto)
+
+print('=' * 20)
+print('O número de caracteres\n(com espaços) é {}'.format(contagem))
+print('=' * 20)
